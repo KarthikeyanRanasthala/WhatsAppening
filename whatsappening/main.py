622adcb35614618fbd6c9594dc4395faa233f163
KarthikeyanRanasthala <karthikeyan.ranasthala@gmail.com>
2019-05-13 06:37:56 +0000

    Create main.py
    A working version with basic features.

---
+++ whatsappening/main.py
@@ -0,0 +1,32 @@
+import datetime
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def main():
+    print('Starting WhatsAppening v1.0.0, It can take a few seconds.')
+    driver = webdriver.Firefox()
+    driver.get('https://web.whatsapp.com')
+    input('\nScan the QR code from Whatsapp Web & Open the contact which you want to track. \n\nPress Enter to continue...')
+    while True:
+        WebDriverWait(driver, 43200).until(
+            EC.visibility_of_element_located(
+                (By.XPATH, '//span[@title="online"]')
+            )
+        )
+        DT = datetime.datetime.now()
+        print('\nOnline  : ' + DT.strftime('%X'))
+        WebDriverWait(driver, 43200).until(
+            EC.invisibility_of_element_located(
+                (By.XPATH, '//span[@title="online"]')
+            )
+        )
+        DT = datetime.datetime.now()
+        print('Offline : ' + DT.strftime('%X'))
+
+
+if __name__ == "__main__":
+    main()
