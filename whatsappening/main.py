2e91b3d9109722fc80018991011e7c317da55a51
KarthikeyanRanasthala <karthikeyan.ranasthala@gmail.com>
2019-05-15 08:06:02 +0000

    Add CSV support
    Tracking Data will be written to a CSV file.

--- whatsappening/main.py
+++ whatsappening/main.py
@@ -5,9 +5,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+def csv(DT, Status):
+    with open('WhatsAppening.csv', 'a') as f:
+        f.write(f'{DT.strftime("%x")},{DT.strftime("%X")},{Status}')
 
 def main():
-    print('Starting WhatsAppening v1.0.0, It can take a few seconds.')
+    print('Starting WhatsAppening v1.1.0, It can take a few seconds.')
     driver = webdriver.Firefox()
     driver.get('https://web.whatsapp.com')
     input('\nScan the QR code from Whatsapp Web & Open the contact which you want to track. \n\nPress Enter to continue...')
@@ -19,6 +22,7 @@ def main():
         )
         DT = datetime.datetime.now()
         print('\nOnline  : ' + DT.strftime('%X'))
+        csv(DT, 'Online\n')
         WebDriverWait(driver, 43200).until(
             EC.invisibility_of_element_located(
                 (By.XPATH, '//span[@title="online"]')
@@ -26,6 +30,7 @@ def main():
         )
         DT = datetime.datetime.now()
         print('Offline : ' + DT.strftime('%X'))
+        csv(DT, 'Offline\n')
 
 
 if __name__ == "__main__":
