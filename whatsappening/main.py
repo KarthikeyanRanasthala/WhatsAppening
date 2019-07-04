a22170dd5f3ff07c1fe4b987ed4e1af45c13fb3b
KarthikeyanRanasthala <karthiikeyan.ranasthala@gmail.com>
2019-07-04 17:38:31 +0530

    Add GeckoDriver Download Support.
    Check for geckodriver executable in current working directory. If unable to locate, then Download the latest version from github.

--- whatsappening/main.py
+++ whatsappening/main.py
@@ -1,13 +1,56 @@
 import datetime
+import os
+import platform
+import sys
+import tarfile
+import zipfile
 from sys import exit
+from urllib.parse import urlparse
 
 import firebase_admin
+import requests
+import tqdm
+from bs4 import BeautifulSoup
 from firebase_admin import credentials, db
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+
+def geckodriverCheck():
+    if not (os.path.isfile('geckodriver') or os.path.isfile('geckodriver.exe')) :
+        print('Unable to locate geckodriver in ' + os.getcwd() + '\n')
+        os_name = platform.system()
+        bit = '64' if sys.maxsize > 2 ** 32 else '32'
+        if os_name == 'Linux':
+            host_os = 'linux' + bit
+        elif os_name == 'Windows':
+            host_os = 'win' + bit
+        elif os_name == 'Darwin':
+            host_os = 'macos'
+        
+        release_url = "https://github.com/mozilla/geckodriver/releases/latest/"
+        release_response = requests.get(release_url)
+        soup = BeautifulSoup(release_response.text, features='html.parser')
+        for a in soup.find_all('a', href = True):
+            if r'/download' and host_os in a['href']:
+                download_url = 'https://www.github.com' + a['href']
+        
+        filename = os.path.split(urlparse(download_url).path)[1]
+        download_url_response = requests.get(download_url, stream = True)
+        with open(filename, 'wb') as f:
+            chunk_size = 1024
+            expeced_size = int(download_url_response.headers['Content-Length'])
+            for chunk in tqdm.tqdm(download_url_response.iter_content(chunk_size), desc= filename, total=expeced_size/chunk_size, unit='kb'):
+                f.write(chunk)
+
+        if filename.lower().endswith('.tar.gz'):
+            with tarfile.open(filename, mode = 'r') as tar:
+                tar.extractall()
+        elif filename.lower().endswith('.zip'):
+            with zipfile.ZipFile(filename, mode = 'r') as zip:
+                zip.extractall()
 
 def csv(DT, Status):
     with open('WhatsAppening.csv', 'a') as f:
@@ -18,7 +61,7 @@ def firebaseAuth():
     try:
         cred = credentials.Certificate('FirebaseAdminSDK.json')
     except FileNotFoundError:
-        print('Unable to find FirebaseAdminSDK.json file in path.')
+        print('Unable to find FirebaseAdminSDK.json file in ' + os.getcwd)
         exit(1)
     firebase_admin.initialize_app(cred, {
         'databaseURL': url
@@ -35,9 +78,10 @@ def firebaseWrite(DT, value):
     })
 
 def main():
-    print('Starting WhatsAppening v1.2.0, It can take a few seconds.')
+    print('Starting WhatsAppening v1.3.0, It can take a few seconds.\n')
+    geckodriverCheck()
     firebaseAuth()
-    driver = webdriver.Firefox()
+    driver = webdriver.Firefox(executable_path='./geckodriver')
     driver.get('https://web.whatsapp.com')
     input('\nScan the QR code from Whatsapp Web & Open the contact which you want to track. \n\nPress Enter to continue...')
     try:
